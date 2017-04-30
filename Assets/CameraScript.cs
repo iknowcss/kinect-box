993ca5811fe0678b0c6d3d67c41715b75ec9d3e8
cjenkins <cjenkins@dius.com.au>
2017-04-30 17:22:14 +1000

    Simple camera motion

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KinectBox.Global;
+
+public class CameraScript : MonoBehaviour
+{
+    private Camera _cam;
+
+    void Start ()
+    {
+        _cam = gameObject.GetComponent<Camera>();
+    }
+	
+	void LateUpdate ()
+    {
+        _cam.transform.position = GameController.Instance.HeadPosition;
+    }
+}
